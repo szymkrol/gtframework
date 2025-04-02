bff5d6bf7f9e91660bf80e0c20a399f240728925
szymek <szymekbukowski05@gmail.com>
2025-04-02 15:21:42 +0200

    dostosowanie evaluate, drobne poprawki wizualne kodu

--- Minmax.py
+++ Minmax.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 import math
 import random
 from Engine import Engine
@@ -8,7 +8,7 @@ from Player import Player
 
 
 class Minmax(Engine):
-    def __init__(self, depth, prun, board: Board, player1: Player,player2: Player):
+    def __init__(self, depth, prun, board: Board, player1: Player, player2: Player):
         self.depth = depth
         self.prun = prun
         self.board = board
@@ -19,11 +19,10 @@ class Minmax(Engine):
         pass
 
     @abstractmethod
-    def evaluate(self,move):
+    def evaluate(self):
         pass
 
     def minimax(self, player, game: Game, iteration=0) -> (int, int):
-
         moves = game.get_available_moves()
         scores = [0 for _ in range(max(moves)+1)]
         if player == self.player1:
@@ -32,40 +31,35 @@ class Minmax(Engine):
             multiplier = -1
         try:
             if iteration != self.depth:
-
                 for i in moves:
                     game.move(i)
                     if player == self.player1:
                         player = self.player2
                     else:
                         player = self.player1
-                    value = self.minimax(player,game,iteration+1)
-                    scores[i]+= value
+                    value = self.minimax(player, game, iteration+1)
+                    scores[i] += value
                     game.revert()
             else:
                 for i in moves:
-                    scores[i] += multiplier*self.evaluate(i)
+                    scores[i] += multiplier*self.evaluate()
         except:
             for i in moves:
-                scores[i] += multiplier * self.evaluate(i)
+                scores[i] += multiplier * self.evaluate()
 
-        best_choice = None
         if player == self.player1:
             best_choice_value = -math.inf
             for i in moves:
-                if scores[i]>best_choice_value:
-                    best_choice = i
+                if scores[i] > best_choice_value:
                     best_choice_value = scores[i]
         else:
             best_choice_value = math.inf
             for i in moves:
-                if scores[i]<best_choice_value:
-                    best_choice = i
-                    best_choice_value= scores[i]
+                if scores[i] < best_choice_value:
+                    best_choice_value = scores[i]
         choices = []
         for i in moves:
             if scores[i] == best_choice_value:
                 choices.append(i)
-        result =  random.choice(choices)
+        result = random.choice(choices)
         return result, scores[result]
-
