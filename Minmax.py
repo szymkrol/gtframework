68243e63ec664f1feeabf68ff191878df7c192bc
szymek <szymekbukowski05@gmail.com>
2025-04-02 21:16:06 +0200

    dodanie funkcji pruning

--- Minmax.py
+++ Minmax.py
@@ -3,23 +3,20 @@ import math
 import random
 from Engine import Engine
 from Game import Game
-from Board import Board
 from Player import Player
 
 
 class Minmax(Engine):
-    def __init__(self, depth, prun, board: Board, player1: Player, player2: Player):
+    def __init__(self, depth, prun, player: Player):
         self.depth = depth
         self.prun = prun
-        self.board = board
-        self.player1 = player1.get_id()
-        self.player2 = player2.get_id()
+        self.player = player.get_id()  # gracz, dla którego minimax liczy ruch
 
-    def run(self, game, move):  # nie za bardzo wiem, jak to uzupełnić
+    def run(self, game, move):
         if self.prun:
-            move = self.minimax(self.player2,game)[0]
+            move = self.minimax(self.player, game)[0]
         else:
-            move = self.pruning(self.player2,game)[0]
+            move = self.pruning(self.player, game)[0]
         Engine.run(self, game, move)
 
     @abstractmethod
@@ -30,7 +27,7 @@ class Minmax(Engine):
         moves = game.get_available_moves()
         n = len(moves)
         scores = [0 for _ in range(n)]
-        if player == self.player1:
+        if player == self.player:
             multiplier = 1
         else:
             multiplier = -1
@@ -39,17 +36,17 @@ class Minmax(Engine):
                 for i in range(n):
                     game.move(moves[i])
                     player = game.get_current_player()
-                    value = self.minimax(player, game, iteration+1)[1]
+                    value = self.minimax(player, game, iteration + 1)[1]
                     scores[i] += value
                     game.revert()
             else:
                 for i in range(n):
-                    scores[i] += multiplier*self.evaluate()
+                    scores[i] += multiplier * self.evaluate()
         else:
             for i in range(n):
                 scores[i] += multiplier * self.evaluate()
 
-        if player == self.player1:
+        if player == self.player:
             best_choice_value = -math.inf
             for i in range(n):
                 if scores[i] > best_choice_value:
@@ -66,5 +63,51 @@ class Minmax(Engine):
         result = random.choice(choices)
         return result, scores[result]
 
-    def pruning(self,player,game,iteration=0,alpha=math.inf,beta= -math.inf):
-        pass
+    def pruning(self, player, game, iteration=0, alpha=-math.inf, beta=math.inf):
+        moves = game.get_available_moves()
+        n = len(moves)
+        scores = [0 for _ in range(n)]
+        if player == self.player:
+            multiplier = 1
+        else:
+            multiplier = -1
+        if (not game.is_finished()) and n > 0:
+            if iteration != self.depth:
+                for i in range(n):
+                    game.move(moves[i])
+                    player = game.get_current_player()
+                    if alpha <= beta:
+                        value = self.pruning(player, game, iteration + 1,alpha,beta)[1]
+                        scores[i] += value
+                        if player == self.player:
+                            if value > alpha:
+                                alpha = value
+                        else:
+                            if value < beta:
+                                beta = value
+                    else:
+                        scores[moves[i]] += -math.inf  # pruned
+                    game.revert()
+            else:
+                for i in range(n):
+                    scores[i] += multiplier * self.evaluate()
+        else:
+            for i in range(n):
+                scores[i] += multiplier * self.evaluate()
+
+        if player == self.player:
+            best_choice_value = -math.inf
+            for i in range(n):
+                if scores[i] > best_choice_value:
+                    best_choice_value = scores[i]
+        else:
+            best_choice_value = math.inf
+            for i in range(n):
+                if scores[i] < best_choice_value:
+                    best_choice_value = scores[i]
+        choices = []
+        for i in range(n):
+            if scores[i] == best_choice_value:
+                choices.append(moves[i])
+        result = random.choice(choices)
+        return result, scores[result]
